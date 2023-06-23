31dde72a8a1b57ca58745a11a482c133c0625d5f
Lalit Upadhyay <118682070+lalit2728@users.noreply.github.com>
2023-06-23 16:35:21 +0000

    conversion2.js

---
+++ basics/conversion2.js
@@ -0,0 +1,22 @@
+// Performing operations to learn implicit type castings;
+// Third implict type casting is interesting;
+
+// -------------------------------------------------------
+// 1.
+let val1 = 1 + "2";
+
+console.log(val1 + " " + typeof(val1));
+
+// -------------------------------------------------------
+// 2. 
+
+let val2 = "1" + 2 + 2;
+
+console.log(val2 + " " + typeof(val2));
+
+// -------------------------------------------------------
+// 3. 
+
+let val3 = 1 + 2 + "2";
+
+console.log(val3);
