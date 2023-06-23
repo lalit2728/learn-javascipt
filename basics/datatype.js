42a3557c92c08a766fe15bc5063659852854ddc2
Lalit Upadhyay <118682070+lalit2728@users.noreply.github.com>
2023-06-23 19:34:39 +0000

    datatype.js

---
+++ basics/datatype.js
@@ -0,0 +1,28 @@
+// Primitive data-types;
+
+// Note: JavaScript is a dynamically-typed language;
+
+/* 
+1. String
+2. Number
+3. Boolean
+4. null
+5. undefined
+6. Symbol
+7. BigInt
+*/
+
+// Reference type / Non-primitive data-types;
+
+/* 
+1. Objects
+2. Arrays
+3. Functions
+*/
+
+// --------------------------------------------
+
+/* How to use Symbol data-type */
+
+
+
